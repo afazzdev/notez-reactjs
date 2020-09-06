7ee005655f7d3d22cd678c52d211cf8fed9ae8d2
PondokProgrammer <afaizi1999@gmail.com>
2020-09-07 01:21:26 +0700

    add icons, add table to fix later

--- packages/wintd-client/src/features/dashboard/Dashboard.tsx
+++ packages/wintd-client/src/features/dashboard/Dashboard.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Grid } from "@material-ui/core";
+import Table, {
+  rowsTest,
+  headCellsTest,
+} from "../../components/tables/EnhancedTable";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <Grid container>
+        <Grid item xs={4}>
+          left side
+        </Grid>
+        <Grid item xs={8}>
+          <Table headCells={headCellsTest} rows={rowsTest} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Dashboard;
