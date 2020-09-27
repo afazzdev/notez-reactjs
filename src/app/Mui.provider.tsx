bf430f1217636ccf71aa74903279c13cef766cab
PondokProgrammer <afaizi1999@gmail.com>
2020-09-28 05:29:29 +0700

    Add secondary color scheme

--- src/app/Mui.provider.tsx
+++ src/app/Mui.provider.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { createMuiTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: "#5068a9",
+      dark: "#86a6df",
+      light: "#324e7b",
+      contrastText: "#f8f8f8",
+    },
+    type: "dark",
+  },
+});
 
 const MuiProvider = ({ children }: { children: React.ReactNode }) => {
   if (process.env.NODE_ENV !== "production") {
     console.log(theme);
+    theme.unstable_strictMode = true;
   }
 
   return (
