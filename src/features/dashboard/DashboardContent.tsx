01a6d7fbbb62861394072512a6cbf652213449fa
PondokProgrammer <afaizi1999@gmail.com>
2020-10-13 23:27:46 +0700

    Add fetch notes

--- src/features/dashboard/DashboardContent.tsx
+++ src/features/dashboard/DashboardContent.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../../app/root.reducer";
 import { ContentDisplay, ContentEditDialog } from "../contents";
-import { editNote } from "../notes/notes.slice";
+import { editNote, getNotesThunk } from "../notes/notes.slice";
 
 function DashboardContent({ maxHeight }: { maxHeight: number }) {
   const notes = useSelector((state: RootState) => state.notes.notes);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(getNotesThunk());
+  }, [dispatch]);
+
+  console.log("dashboard content rerender");
   return (
     <div
       style={{
