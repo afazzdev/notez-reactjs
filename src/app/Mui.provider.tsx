a52d95b2cc438c7276dc3dfc9cf47d7847840739
PondokProgrammer <afaizi1999@gmail.com>
2020-10-14 20:30:46 +0700

    Add style to scroll

--- src/app/Mui.provider.tsx
+++ src/app/Mui.provider.tsx
@@ -11,6 +11,32 @@ const theme = createMuiTheme({
     },
     type: "dark",
   },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        "*": {
+          "&::-webkit-scrollbar": {
+            width: 10,
+          },
+
+          /* Track */
+          "&::-webkit-scrollbar-track": {
+            background: "#f1f1f1",
+          },
+
+          /* Handle */
+          "&::-webkit-scrollbar-thumb": {
+            background: "#888",
+          },
+
+          /* Handle on hover */
+          "&::-webkit-scrollbar-thumb:hover": {
+            background: "#555",
+          },
+        },
+      },
+    },
+  },
 });
 
 const MuiProvider = ({ children }: { children: React.ReactNode }) => {
