b1c36a28dca301882fe2b196c4616585e3e9f810
PondokProgrammer <afaizi1999@gmail.com>
2020-10-15 22:28:32 +0700

    Add style for favorite note

--- src/features/contents/ContentDisplay.tsx
+++ src/features/contents/ContentDisplay.tsx
@@ -1,35 +1,52 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Tag from "../../components/tag/Tag";
+import clsx from "clsx";
 
-const useStyles = makeStyles({
-  root: {
-    width: "100%",
-    margin: "0 1rem 1rem 0",
-    display: "inline-block",
-  },
-  title: {
-    overflow: "hidden",
-    WebkitLineClamp: 2,
-    WebkitBoxOrient: "vertical",
-    display: "-webkit-box",
-    whiteSpace: "break-spaces",
-    wordWrap: "break-word",
-  },
-  content: {
-    overflow: "hidden",
-    WebkitLineClamp: 20,
-    WebkitBoxOrient: "vertical",
-    display: "-webkit-box",
-    whiteSpace: "break-spaces",
-    wordWrap: "break-word",
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: ({ favorite }: { favorite: boolean }) => ({
+      width: "100%",
+      margin: "0 1rem 1rem 0",
+      display: "inline-block",
+      position: "relative",
+    }),
+    title: {
+      overflow: "hidden",
+      WebkitLineClamp: 2,
+      WebkitBoxOrient: "vertical",
+      display: "-webkit-box",
+      whiteSpace: "break-spaces",
+      wordWrap: "break-word",
+    },
+    content: {
+      overflow: "hidden",
+      WebkitLineClamp: 20,
+      WebkitBoxOrient: "vertical",
+      display: "-webkit-box",
+      whiteSpace: "break-spaces",
+      wordWrap: "break-word",
+    },
+    favorite: {
+      "&::after": {
+        content: `""`,
+        height: 30,
+        width: 30,
+        backgroundColor: theme.palette.secondary.main,
+        position: "absolute",
+        top: 0,
+        right: 0,
+        clipPath: "polygon(100% 0, 100% 50%, 50% 100%, 0 50%, 0 0)",
+        transform: "translate(-10px, -5px)",
+      },
+    },
+  }),
+);
 
 export interface IContentDisplayProps {
   image?: string;
@@ -37,6 +54,7 @@ export interface IContentDisplayProps {
   content?: string;
   tags?: any[];
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  favorite?: boolean;
 }
 
 export default function ContentDisplay({
@@ -45,11 +63,12 @@ export default function ContentDisplay({
   content,
   tags,
   onClick,
+  favorite,
 }: IContentDisplayProps) {
-  const classes = useStyles();
+  const classes = useStyles({ favorite: favorite! });
 
   return (
-    <Card className={classes.root}>
+    <Card className={clsx(classes.root, { [classes.favorite]: favorite })}>
       <CardActionArea onClick={onClick}>
         {image && (
           <CardMedia
